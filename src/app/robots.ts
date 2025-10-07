287c765c5ee0f4890b1cf4ba47cb621c3c9e1e4d
Manohar Mankala <manohar_mankala@anfcorp.com>
2025-10-06 22:34:12 -0400

    sitemap and robots updates

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,9 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 
-    (process.env.NODE_ENV === 'production' 
-      ? 'https://evite.mankala.space' 
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ||
+    (process.env.NODE_ENV === 'production'
+      ? 'https://evite.mankala.space'
       : 'http://localhost:3008');
 
   return {
